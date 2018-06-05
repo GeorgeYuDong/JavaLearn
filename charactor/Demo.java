e4edc160a054c6c5e5ec1c9e75db79440d6dc436
GeorgeYuDong <nanjing1222@163.com>
2018-06-05 10:53:48 +0800

    add first step and confidence message

---
+++ charactor/Demo.java
@@ -0,0 +1,20 @@
+package charactor;
+
+/**
+ * @Author:YuDong
+ * @Description:
+ * @Date:created in 10:47 2018/6/5
+ * @Modified By:
+ */
+
+public class Demo {
+    //尽快迈出第一步
+    //尽快建立自信和成就感,让自己有勇气走下去
+    //然后缺什么，补什么
+    //真正让人望而却步的不是技术本身，而是对自身的不自信
+    //唯有果断开工，才能战胜心魔
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
