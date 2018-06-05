edb297728fb8f450743e2c3187ad6b81eb9417d9
GeorgeYuDong <nanjing1222@163.com>
2018-06-05 11:35:01 +0800

    delete wave line

--- charactor/Demo.java
+++ charactor/Demo.java
@@ -7,12 +7,13 @@ package charactor;
  * @Modified By:
  */
 
-public class Demo {
+public class Demo extends Object{
     //尽快迈出第一步
     //尽快建立自信和成就感,让自己有勇气走下去
     //然后缺什么，补什么
     //真正让人望而却步的不是技术本身，而是对自身的不自信
     //唯有果断开工，才能战胜心魔
+    //默认导入了类import java.lang.*，包含System,Object,String,Math等
 
     public static void main(String[] args) {
         System.out.println("hello world");
--- charactor/GiantDragon.java
+++ charactor/GiantDragon.java
@@ -9,22 +9,28 @@ package charactor;
 
 public class GiantDragon {
     //私有化构造方法,使得无法在外部通过new进行实例化
+    //注释与方法或属性隔一行，就没有破浪线
+
     private GiantDragon(){
     }
     //饿汉模式
     //实例化一个类属性,指向一个实例化对象，因为是类属性,所以只有一个
     //单例模式，无论如何都会创建一个
+
     private static GiantDragon instance = new GiantDragon();
 
     //懒汉模式
+
     private static GiantDragon in;
 
     //返回一个类属性实例
+
     public static GiantDragon getInstance(){
         return instance;
     }
 
     //懒汉模式,与饿汉模式不一样在于，类引用先是null.在调用方法时,判断是否已有对象实例，没有创建。
+
     public static GiantDragon getIn(){
         if (null == in){
             in = new GiantDragon();
