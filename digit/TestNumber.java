a1553f377bf751bac15a4c0555d8177726238546
GeorgeYuDong <nanjing1222@163.com>
2018-06-06 21:03:19 +0800

    add integer message

---
+++ digit/TestNumber.java
@@ -0,0 +1,22 @@
+package digit;
+
+/**
+ * @Author:YuDong
+ * @Description:
+ * @Date:created in 20:49 2018/6/6
+ * @Modified By:
+ */
+
+public class TestNumber {
+    public static void main(String[] args) {
+        int i = 5;
+        //构造方法，把属性值value初始化
+        Integer it = new Integer(i);
+        System.out.println(it);
+
+        int i2 = it.intValue();
+        System.out.println(i2);
+
+
+    }
+}
