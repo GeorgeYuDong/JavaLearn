e939649cfbcb13641c332eccf14197520ac85d37
GeorgeYuDong <nanjing1222@163.com>
2018-06-19 11:54:00 +0800

    add message

--- digit/TestNumberToString.java
+++ digit/TestNumberToString.java
@@ -13,7 +13,7 @@ public class TestNumberToString {
 
         //第一种
         //数字转字符串，String类的静态valueOf方法
-        //valueOf静态方法，不需要使用new创建对象
+        //valueOf静态方法，不需要使用new创建对象,调用Integer.toString()静态方法
         //一般是 工具类，使用静态方法,不用创建对象
         String str = String.valueOf(i);
         System.out.println(str);
@@ -22,6 +22,13 @@ public class TestNumberToString {
         //先装箱为对象，再调用toString方法
         Integer it = i;
         String str2 = it.toString();
+
+        System.out.println(str2);
+        //无为null,没有为空字符"",老子之无
+        //Java程序员上班的那些事儿
+
+
+        
 
     }
 }
